0342dd5d6a0b07d2ea46fa84f72aaae7e09a62fd
Vigolix <33756328+Vigolix@users.noreply.github.com>
2017-11-30 13:02:45 +0100

    Update eval_rankings.py

--- S4/eval_rankings.py
+++ S4/eval_rankings.py
@@ -22,13 +22,13 @@ def display(params,query_id,ranking,relnotrel):
 
 
 
-    # Read query image
+    # llegeix les imatges de consulta
 
     query_im =  cv2.imread(os.path.join(params['root'],params['database'],params['split'], 'images',query_id.split('.')[0] + '.jpg'))
 
 
 
-    # Handling the duality in file terminations. I know it's not pretty, but it works...
+    # com els fitxers tenen una terminacio .jpg em de trobar una manera per no contar aquesta terminacio. 
 
     if query_im is None:
 
@@ -36,7 +36,7 @@ def display(params,query_id,ranking,relnotrel):
 
 
 
-    # Blue contour for the query
+    # utilitzem el contorn blau per la consulta
 
 
 
@@ -44,7 +44,7 @@ def display(params,query_id,ranking,relnotrel):
 
     query_im = cv2.copyMakeBorder(query_im,100,100,100,100,cv2.BORDER_CONSTANT,value=[0,0,255])
 
-    # Init figure
+    # inicialitza la figura
 
     fig = plt.figure(figsize=(20,10))
 
@@ -52,7 +52,7 @@ def display(params,query_id,ranking,relnotrel):
 
 
 
-    # Display
+    # es visualitzen els resultats
 
     ax.imshow(query_im)
 
@@ -64,13 +64,13 @@ def display(params,query_id,ranking,relnotrel):
 
 
 
-    # We will show the first 15 elements of the ranking
+    # mostrarem el 15 primers elements del ranking
 
     for i in range(15):
 
 
 
-        # Read image
+        #llegeix la imatge
 
 
 
@@ -78,7 +78,6 @@ def display(params,query_id,ranking,relnotrel):
 
 
 
-        # Handling the duality in file terminations. I know it's not pretty, but it works...
 
         if im is None:
 
@@ -88,37 +87,36 @@ def display(params,query_id,ranking,relnotrel):
 
 
 
-        # Switch to RGB to display with matplotlib
+        # canviem la visualitzacio a RGB amb matplotlib
 
         im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
 
 
 
-        # Paint the boundaries with the ground truth
+        # pintem el limits amb uns
 
 
-
-        # If it was correctly selected
+        # si es selecciona correctament
 
         if relnotrel[i] == 1:
 
-            # Put green contour
+            # posem el contorn verd
 
             im = cv2.copyMakeBorder(im,100,100,100,100,cv2.BORDER_CONSTANT,value= [0,255,0])
 
 
 
-        # If it was not
+        # si no es compleix la condicio aleshores,
 
         else:
 
-            # Put red contour
+            # posem el contorn vermell
 
             im = cv2.copyMakeBorder(im,100,100,100,100,cv2.BORDER_CONSTANT,value= [255,0,0])
 
 
 
-        # Show in figure
+        # mostrem la figura
 
         ax = fig.add_subplot(4, 4, i+2)
 
@@ -140,7 +138,7 @@ def read_annotation(params):
 
 
 
-    # Get true annotations
+    # agafem les anotacions correctes
 
     annotation_val = pd.read_csv(os.path.join(params['root'],params['database'],params['split'],'annotation.txt'), sep='\t', header = 0)
 
@@ -154,31 +152,31 @@ def read_annotation(params):
 def get_hitandmiss(ranking,query_class,annotation_train):
 
 
-    # Initialize hit/miss list
+    # Inicialitzem la llista hit/miss 
 
     relnotrel = []
 
 
 
-    # For each image id in the ranking...
+    # per a cada id d'imatge en el ranking
 
     for i in ranking[0].tolist():
 
 
 
-        # Get its class from the training annotations
+        # agafem la clase de les anotacions
 
         i_class = list(annotation_train.loc[annotation_train['ImageID'] == i]['ClassID'])[0]
 
 
 
-        # And if it matches the query class...
+        # si coincideix amb alguna clase de la consulta
 
         if query_class == i_class:
 
 
 
-            # Then it means it's correct
+            # significa que es correcta
 
             relnotrel.append(1)
 
@@ -186,7 +184,7 @@ def get_hitandmiss(ranking,query_class,annotation_train):
 
 
 
-            # If it doesn't, then we failed :(
+            # si no es que em fallat
 
             relnotrel.append(0)
 
@@ -202,43 +200,43 @@ def AveragePrecision(relist):
 
 
 
-    # Initialize the accumulated sum of precisions
+    # inicialitzem la suma de precisions acumulada
 
     accu = 0
 
 
 
-    # Initialize the number of correct instances found so far
+    # inicialitzem el nombre correcte de coincidencies trobades
 
     numRel = 0
 
 
 
-    # For all elements in the hit & miss list
+    # per a tots el elements a la llista hit & miss 
 
     for k in range(len(relist)):
 
 
 
-        # If the value is 1
+        # si el valor es 1
 
         if relist[k] == 1:
 
 
 
-            # We add 1 to the number of correct instances
+            # afegim un 1 al nombre de coincidencies correctes
 
             numRel = numRel + 1
 
 
 
-            # We calculate the precision at k (+1 because we start at 0) and we accumulate it
+            # calculem la precisio a K(+1 per que comencem 0) i ho acumulem
 
             accu += float( numRel )/ float(k+1)
 
 
 
-    # When we finish, we divide by the total number of relevant instances, which is the sum of ones in the list
+    # quan em acavbat,dividim el nombre total de coincidencies rellevants, que es la suma dels 1 de la llista
 
     return (accu/np.sum(relist))
 
@@ -252,13 +250,13 @@ def load_ranking(params,query_id, annotation_val):
 
 
 
-    # Get the true class of the validation image for which we will evaluate the ranking
+    # obtenim la verdadera clase de la validacio de l'imatge(totes les que em evaluat del ranking) 
 
     query_class = list(annotation_val.loc[annotation_val['ImageID'] == query_id.split('.')[0]]['ClassID'])[0]
 
 
 
-    # Open its ranking file
+    # obre el fitxer ranking
 
     ranking = pd.read_csv(os.path.join(params['root'],params['root_save'],params['rankings_dir'],params['descriptor_type'],params['split'],query_id.split('.')[0] + '.txt'),header= None)
 
@@ -288,19 +286,19 @@ def save_ranking_file(file_to_save,image_id,ranking):
 
 
 
-    # Write query name
+    # escriu un nom de consulta
 
     file_to_save.write(image_id.split('.')[0] + ',')
 
 
 
-    # Convert elements to string and ranking to list
+    # converteix l'element a un string i el posa a la llista ranking
 
     ranking = np.array(ranking).astype('str').tolist()
 
 
 
-    # Write space separated ranking
+    # escriu un espai separat al ranking
 
     for item in ranking:
 
@@ -324,7 +322,7 @@ def eval_rankings(params):
 
 
 
-    # Prepare to save kaggle friendly file with all rankings
+    # prepara per guardar el kaggle amb tots els rankings
 
     if params['save_for_kaggle']:
 
@@ -334,19 +332,19 @@ def eval_rankings(params):
 
 
 
-        # Write first line with header
+        # escriu la primera linia amb la capçalera
 
         file_to_save.write("Query,RetrievedDocuments\n")
 
 
 
-    # Create a dictionary to store the accumulated AP for each class
+    # crea un diccionari per emmagatzemar el AP acumulat per cada clase
 
     dict_ = {key: 0 for key in params['possible_labels']}
 
 
 
-    # Get true annotations
+    # agafa les anotacions correctes
 
     annotation_val, annotation_train = read_annotation(params)
 
@@ -362,7 +360,7 @@ def eval_rankings(params):
 
 
 
-    # For all generated rankings
+    # per a tots el ranking generats
 
     for val_id in os.listdir(os.path.join(params['root'],params['root_save'],params['rankings_dir'],params['descriptor_type'],params['split'])):
 
@@ -372,7 +370,7 @@ def eval_rankings(params):
 
 
 
-        # We do not evaluate the queries in the unknown class !
+        # no evaluem les consultes de clases desconegudes
 
         if not query_class == "desconegut":
 
@@ -386,13 +384,13 @@ def eval_rankings(params):
 
 
 
-            # Get the hit & miss list
+            # obtenim la llista hit & miss 
 
             relnotrel = get_hitandmiss(ranking,query_class,annotation_train)
 
 
 
-            # Calculate average precision of the list
+            # Calcula el promig de precisio de la llista 
 
             ap = AveragePrecision(relnotrel)
 
@@ -400,13 +398,12 @@ def eval_rankings(params):
 
 
 
-            # OPTIONAL: Add the AP to the according dictionary entry
-
+           
             dict_[query_class] += ap
 
 
 
-            # Store it
+            # guardem
 
             ap_list.append(ap)
 
@@ -430,31 +427,30 @@ def single_eval(params,query_id):
 
 
 
-    # We get the true annotations of both sets. We do this in order to display this information as well.
+    # obtenim les anotacions correctes.
 
     annotation_val, annotation_train = read_annotation(params)
 
 
 
-    # We get the ranking and the true class of the query
+    # obtenim el ranking i les clases correctes de les consultes
 
     query_class, ranking = load_ranking(params,query_id,annotation_val)
 
 
 
-    # I made sure I was not picking an image from the "desconegut" class ...
-
+    # aixi m'aseguro de que no agafo una imatge de clase desconeguda
     print query_class
 
 
 
-    # The ranking is composed of the training images. It should be of size 450.
+    # el ranking el composem amb aquestes imatges. hauria de ser d'una mida de 450.
 
     print len(ranking)
 
 
 
-    # We get the hit/miss list for the ranking
+
 
     relnotrel = get_hitandmiss(ranking,query_class,annotation_train)
 
@@ -484,6 +480,6 @@ if __name__ == "__main__":
 
         if not id == 'desconegut':
 
-            # We divide by 10 because it's the number of images per class in the validation set.
+            # ho dividim per 10 ha que es el nombre d'iamatges per clase a la validacio
 
             print id+':', dict_[id]/10
